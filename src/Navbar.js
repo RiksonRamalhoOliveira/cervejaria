dd283353c55574bbab29a86dc1ad9858c657048a
RiksonRamalhoOliveira <riksonunipe@gmail.com>
2019-09-11 06:02:36 -0300

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
-import imageLogo from '../src/imgs/logo.png';
 
 class Navbar extends Component {
   render() {
       return(
         <div className="App boxShadow">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
-          <a className="navbar-brand" href="/">
-            <img src={imageLogo} />       
-          </a>
-        </nav>
+          <h5>Cervejaria UNIPE</h5>
         
       </div>  )
   }
